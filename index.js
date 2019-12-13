df59c62b0f1d66098714b52bc02c56f4336ed9a3
JacobLayton <jlayjlay3@gmail.com>
2019-12-12 16:36:24 -0800

    Index.js updated and server is up an running, imported DBs

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code away!
+const server = require("./server");
+
+server.listen(5002, () => {
+  console.log("\n*** Server is running on port 5002 ***\n");
+})
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 
+const userDb = require("./users/userDb");
+const postDb = require("./posts/postDb");
+
 const server = express();
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) { }
 
 module.exports = server;
